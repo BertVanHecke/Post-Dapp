ad37e3484d7be7843df47468d2ba00f240ede0f7
bertvanheckee <bert.vanhecke@student.vives.be>
2023-01-19 21:21:43 +0100

    ipfs setup & dotenv

---
+++ client/src/ipfs/ipfs.js
@@ -0,0 +1,21 @@
+import { create } from "ipfs-http-client";
+
+const projectId = process.env.REACT_APP_INFURA_PROJECT_ID;
+const projectSecret = process.env.REACT_APP_INFURA_SECRET_KEY;
+
+const authorization = `Basic ${Buffer.from(
+  `${projectId}:${projectSecret}`
+).toString("base64")}`;
+
+export let ipfs;
+try {
+  ipfs = create({
+    url: "https://ipfs.infura.io:5001/api/v0",
+    headers: {
+      authorization,
+    },
+  });
+} catch (error) {
+  console.error("IPFS error ", error);
+  ipfs = undefined;
+}
